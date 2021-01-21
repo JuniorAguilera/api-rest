019a78c5ea38acd7f0ca2f3cbcb24f6542446423
Junior Aguilera <junior.aguilera.alvarado@gmail.com>
2021-01-20 23:25:42 -0500

    update new change

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,14 +2,6 @@ import { Router } from "express"
 
 const router = Router()
 
-<<<<<<< HEAD
 router.get('/',(req,res)=>res.json('get products'))
 
 export default router;
-=======
-import * as productCtrl from '../controllers/products.controller'
-
-router.get('/',(req,res)=>res.json('get products'))
-
-export default router;
->>>>>>> develop
